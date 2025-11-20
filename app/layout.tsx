1a1dd746ddc94e19580791a62be6ebceb444668c
SK MD EKBAL <md.ekbal@senrysa.com>
2025-11-20 16:02:40 +0530

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,16 @@ export default function RootLayout({
        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""></link>
        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+       <meta property="og:title" content="Uniline - Customizable Icon Library" />
+  <meta property="og:description" content="A library of customizable icons, available as React components, CSS icon font, or via CDN. Supports size, color, stroke width, and filled variants." />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://unilineicon.netlify.app" />
+  <meta property="og:image" content="/logo192.png" />
+  <meta property="og:site_name" content="Uniline" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Uniline - Customizable Icon Library" />
+  <meta name="twitter:description" content="A library of customizable icons, available as React components, CSS icon font, or via CDN. Supports size, color, stroke width, and filled variants." />
+  <meta name="twitter:image" content="/logo192.png" />
         {/* Alternative: Add via Google Fonts similar font */}
         <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;family=Mozilla+Headline:wght@200..700&amp;display=swap" rel="stylesheet"></link>
 
